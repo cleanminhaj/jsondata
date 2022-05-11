0c14075b07758cef260e07c74f6652433c130420
Md Minhaj <minhajallsocial@gmail.com>
2022-05-11 13:14:39 +0600

    json data done

---
+++ app.js
@@ -0,0 +1,42 @@
+// /**
+//  * JSON data
+//  */
+
+const devs = [
+  {
+     id : 1,
+     name : "Minhaj",
+     age :20,
+     skill :"PHP",
+  },
+
+  {
+    id : 2,
+    name : "Munsur",
+    age :22,
+    skill :"Laravel",
+ },
+
+ {
+    id : 3,
+    name : "Milon",
+    age :27,
+    skill :"Python",
+ },
+
+{
+     id : 4,
+     name : "Ismail",
+     age :28,
+     skill :"Blockchain",
+  },
+
+  {
+    id : 5,
+    name : "Naim",
+    age :20,
+    skill :"Jango",
+ }
+
+];
+
